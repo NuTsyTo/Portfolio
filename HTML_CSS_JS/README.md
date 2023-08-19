174b9b1b59bf8e387403baf464c8e908c0285880
NuTsyTo <maxence.degaugue@gmail.com>
2023-08-19 13:31:31 +0200

    Commit des fichiers nécessaire pour mon portfolio

---
+++ HTML_CSS_JS/README.md
@@ -0,0 +1,3 @@
+# portfolio
+
+Vous trouverez ci-joint tout les fichier nécessaire à la création de mon portfolio.
---
+++ HTML_CSS_JS/script.js
@@ -0,0 +1,8 @@
+<script>
+        const menuHamburger = document.querySelector("...")
+        const navLinks = document.querySelector("...")
+ 
+        menuHamburger.addEventListener('click',()=>{
+        navLinks.classList.toggle('mobile-menu')
+        })
+</script>
